4c97670294af45fdeb830b1017564cfe7405b656
Doro-nyong / 도로뇽 <doronyong94@gmail.com>
2019-07-17 01:17:35 +0900

    add: template engine for works_list

--- script.js
+++ script.js
@@ -1,47 +1,102 @@
 /*
+* Works Data
+*/
+
+const works = {
+    visualDev: [
+        {
+            position: "left",
+            thumbnail: {
+                image: "https://placehold.it/800x800",
+                type: "square" 
+            }
+        },
+        {
+            position: "left",
+            thumbnail: {
+                image: "https://placehold.it/800x800",
+                type: "rectangle_hor" 
+            }
+        },
+        {
+            position: "right",
+            thumbnail: {
+                image: "https://placehold.it/800x800",
+                type: "rectangle_hor" 
+            }
+        },
+        {
+            position: "right",
+            thumbnail: {
+                image: "https://placehold.it/800x800",
+                type: "rectangle_ver" 
+            }
+        }
+    ],
+    illustration: [],
+    storytelling: []
+}
+
+
+
+/*
 * SPA
 */
 
 const menu = {
     main: {
         link: document.getElementById('menu_main'),
-        contents: document.getElementById('main'),
-        bodyStatus: "status_main_menu"
+        section: document.getElementById('main'),
+        bodyStatus: "status_main_menu",
+        contents: null
     },
     about: {
         link: document.getElementById('menu_about'),
-        contents: document.getElementById('about'),
-        bodyStatus: "status_about"
+        section: document.getElementById('about'),
+        bodyStatus: "status_about",
+        contents: null
     },
-    works1: {
-        link: document.getElementById('menu_works_1'),
-        contents: document.getElementById('works_1'),
-        bodyStatus: "status_works"
-    },
-    works2: {
-        link: document.getElementById('menu_works_2'),
-        contents: document.getElementById('works_2'),
-        bodyStatus: "status_works"
-    },
-    works3: {
-        link: document.getElementById('menu_works_3'),
-        contents: document.getElementById('works_3'),
+    worksList: {
+        link: [
+            document.getElementById('menu_works_list_1'),
+            document.getElementById('menu_works_list_2'),
+            document.getElementById('menu_works_list_3')
+        ],
+        section: document.getElementById('works_list'),
+        contents: [
+            {
+                title: "Visual Dev.",
+                list: works.visualDev
+            },
+            {
+                title: "Illustration",
+                list: works.illustration
+            },
+            {
+                title: "Storytelling",
+                list: works.storytelling
+            }
+        ],
         bodyStatus: "status_works"
     }
 }
+
+const worksListHeadline = document.querySelector("#works_list .works_headline h2");
+const worksListButtonBack = document.querySelector("#works_list a.button_back");
+
 
 let currentMenu = menu.main;
 
 const menuChange = function(target) {
     if (target === currentMenu) { return; }
 
-    currentMenu.contents.classList.add('opacity0');
+    currentMenu.section.classList.add('opacity0');
     setTimeout( function() {
-        currentMenu.contents.classList.add('displaynone');
-        target.contents.classList.remove('displaynone');
+        currentMenu.section.classList.add('displaynone');
+        target.section.classList.remove('displaynone');
     }, 200);
     setTimeout( function() {
-        target.contents.classList.remove('opacity0');
+        target.section.classList.remove('opacity0');
     }, 300);
 
     document.body.classList.remove(currentMenu.bodyStatus);
@@ -50,8 +105,55 @@ const menuChange = function(target) {
     setTimeout( function() { currentMenu = target; }, 200);
 }
 
+
+const worksListLeft = document.querySelector("#works_list section:first-of-type article");
+const worksListRight = document.querySelector("#works_list section:last-of-type article");
+
+
+const worksListContentsAdd = function(worksListTarget, contents) {
+    for (let i = 0; i < contents.length; i++) {
+        let worksListItem = document.createElement("a");
+        let worksListItemThumbnail = document.createElement("img");
+
+        worksListItem.setAttribute('class', contents[i].thumbnail.type);
+        worksListItem.setAttribute('href', '#');
+
+        worksListItemThumbnail.setAttribute('src', contents[i].thumbnail.image);
+
+        worksListItem.appendChild(worksListItemThumbnail);
+        worksListTarget.appendChild(worksListItem);
+    }
+}
+
+
+const contentsChange = function(target, contents) {
+    if ( target === "worksList" ) {
+        worksListHeadline.innerHTML = contents.title;
+
+        let worksListLeftOnly = contents.list.filter( function( item ) { return item.position === "left"; } );
+        let worksListRightOnly = contents.list.filter( function( item ) { return item.position === "right"; } );
+
+        worksListLeft.innerHTML = "";
+        worksListRight.innerHTML = "";
+
+        worksListContentsAdd(worksListLeft, worksListLeftOnly);
+        worksListContentsAdd(worksListRight, worksListRightOnly);
+
+    } else if ( target === "worksView" ) {
+        return 0;
+
+    } else {
+        console.error("Error! param of contentsChange: target '" + target + "' is not the right type.");
+        
+    }
+}
+
+
 menu.main.link.addEventListener('click', function() { menuChange(menu.main); });
 menu.about.link.addEventListener('click', function() { menuChange(menu.about); });
-menu.works1.link.addEventListener('click', function() { menuChange(menu.works1); });
-menu.works2.link.addEventListener('click', function() { menuChange(menu.works2); });
-menu.works3.link.addEventListener('click', function() { menuChange(menu.works3); });
+
+worksListButtonBack.addEventListener('click', function() { menuChange(menu.main); });
+
+menu.worksList.link[0].addEventListener('click', function() { menuChange(menu.worksList); contentsChange("worksList", menu.worksList.contents[0]); });
+menu.worksList.link[1].addEventListener('click', function() { menuChange(menu.worksList); contentsChange("worksList", menu.worksList.contents[1]); });
+menu.worksList.link[2].addEventListener('click', function() { menuChange(menu.worksList); contentsChange("worksList", menu.worksList.contents[2]); });
